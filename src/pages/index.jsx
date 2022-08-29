30be931aaaf5ae453fc90e0ff577fbde82a62d0b
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-08-29 16:45:54 +0800

    Move styles and pages folder to src

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/nextjs-icon-dark.svg" />
         <title>Title</title>
       </Head>
     </div>

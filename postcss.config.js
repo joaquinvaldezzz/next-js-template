bd22e6263197b55a8c0ab9e4695c54dff76a10c7
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-08-29 16:47:17 +0800

    Delete empty line

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,6 @@ module.exports = {
         const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
         return broadMatches.concat(innerMatches)
       },
-
       keyframes: true,
       variables: true,
       safelist: ['html', 'body'],

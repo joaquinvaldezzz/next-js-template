63f8f8cde76e0906ec325b96061bd1416faa559e
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-24 14:35:39 +0800

    Turn off "react/jsx-props-no-spreading"

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '../styles/main.css'
 
 function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
